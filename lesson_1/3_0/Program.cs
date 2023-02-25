71ef7de271ab2540a35068e8fe1a830808bb6149
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-02-26 00:07:24 +0300

    Task 3_0

--- lesson_1/3_0/Program.cs
+++ lesson_1/3_0/Program.cs
@@ -1,2 +1,37 @@
 ﻿// 2. Напишите программу, которая будет выдавать название дня
 // недели по заданному номеру.
+
+string s_a = Console.ReadLine()!;
+
+if (s_a == "1")
+{
+  Console.WriteLine("Пн");
+}
+if (s_a == "2")
+{
+  Console.WriteLine("Вт");
+}
+if (s_a == "3")
+{
+  Console.WriteLine("Ср");
+}
+if (s_a == "4")
+{
+  Console.WriteLine("Чт");
+}
+if (s_a == "5")
+{
+  Console.WriteLine("Пт");
+}
+if (s_a == "6")
+{
+  Console.WriteLine("Сб");
+}
+if (s_a == "7")
+{
+  Console.WriteLine("Вс");
+}
+else
+{
+  Console.WriteLine("Error");
+}
