962a3bb0d41c28f27d949e6f70183530f6de25a4
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-02-26 14:46:09 +0300

    HW 1

---
+++ lesson_1/HW/HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1.
+//Написать программу, которая принимает два числа и на выходе выдает, какое наибольшее, а какое – наименьшее
+
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+  Console.WriteLine(a + "=max");
+}
+else if (a < b)
+{
+  Console.WriteLine(b + "=max");
+}
+else
+{
+  Console.WriteLine("Error");
+}
