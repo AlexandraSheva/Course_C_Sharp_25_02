3716e8981a4daa92737bda25a34cb275768c798e
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-02-26 15:53:13 +0300

    HW 4

---
+++ lesson_1/HW/HW_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4. 
+//Написать программу, которая принимает число N и выдает все четные от 1 до N
+
+string s_n = Console.ReadLine()!;
+int n = int.Parse(s_n);
+int a = 2;
+
+while (a <= n)
+{
+  Console.Write(a);
+  a = a + 2;
+}
