fa8c250f62d1cd033b17bff5dafcb9e8ee54c8e7
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-02 22:59:45 +0300

    HW 3

---
+++ lesson_2/HW/HW_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3. Написать программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int Find(int num)
+{
+  if (num < 100)
+  {
+    Console.WriteLine("Нет третьего числа");
+  }
+  else if (num > 99 && num < 1000)
+  {
+    Console.WriteLine(num % 10);
+  }
+}
+
+int result = Find(int.Parse(Console.ReadLine()));
+Console.WriteLine(result);
+
