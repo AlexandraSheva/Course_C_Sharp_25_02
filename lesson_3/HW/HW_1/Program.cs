47381d9f92366bcf138370346922028069aed8cc
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-04 20:33:54 +0300

    HW 1

---
+++ lesson_3/HW/HW_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1. Написать программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом
+
+void Palindrome(int num)
+{
+  if (num > 9999 && num < 100000 && num / 10000 == num % 10 && num % 10000 / 1000 == num % 100 / 10)
+  {
+    Console.WriteLine("Yes");
+  }
+  else Console.WriteLine("No");
+}
+
+int x = int.Parse(Console.ReadLine()!);
+Palindrome(x);
