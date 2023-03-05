984be5b25819d58f4bc05c55c3ed278fd1f6cc7c
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-05 14:35:25 +0300

    Task HW 1

---
+++ lesson_4/HW/HW_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать цикл, который принимает на вход два числа (А и В) и возводит число А 
+// в натуральную степень числа В. 
+
+//Понимаю, что сделала задачу неверно, но не знаю, как по-другому ее еще можно решить. 
+
+int Stepen(int num1, int num2)
+{
+  for (num1 > 0; num2 > 0;)
+  {
+    return Math.Pow(num1, num2);
+  }
+}
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Stepen(A,B));
