e98024e73bd53dbe826658d494d504430bb165a2
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-05 14:58:10 +0300

    Task HW 2

--- lesson_4/Task_4_1/Program.cs
+++ lesson_4/Task_4_1/Program.cs
@@ -1,9 +1,11 @@
 ﻿// 1. Напишите программу, которая принимает
 //    на вход число и выдаёт количество цифр в числе.
 
-int SumNum(int num){
+int SumNum(int num)
+{
     int kol =0;
-    for(int i = 1 ; num > 0 ; i++){
+    for(int i = 1 ; num > 0 ; i++)
+    {
         num = num / 10;
         kol = i;
     }
