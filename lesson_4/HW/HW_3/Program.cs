60d86353f26d220131294c87c97638a94f237c05
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-05 15:08:52 +0300

    Task HW 3

---
+++ lesson_4/HW/HW_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3. Написать программу, которая задает массив из 8 элементов случайными числами и выводит их на экран
+
+void Array(int num)
+{
+  int[] Ar = new int[num];
+  for(int i=0;i<num;i++)
+  {
+    Ar[i]=new Random().Next(10);
+    Console.WriteLine(Ar[i]);
+  }
+}
+
+Array(8);
