07147c798e0f5d36787d912afe06ce9539da50a8
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-17 23:58:08 +0300

    HW_5_1

---
+++ lesson_5/HW/HW_5_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1. Задать массив, заполненный случайными положительными трехзначными числами.
+//Написать программу, которая покажет колличество четных чисел в массиве. 
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int SumPosNeg(int[] arr)
+{
+    int num_2 = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2==0)
+            num_2++;
+
+    }
+    return num_2;
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = 100;
+int stop = 999;
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine (SumPosNeg(mass));
+
