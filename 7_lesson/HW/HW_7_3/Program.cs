db25ea27818a797cc2659cdd593f29bd4837cc9f
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-18 20:03:44 +0300

    HW_7_3

---
+++ 7_lesson/HW/HW_7_3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 3. Задать двумерный массив из целых чисел
+// Найти среднее арифметическое каждого в каждом столбце
+
+//Застряла на третьем методе. Не знаю, как его правильно оформить. 
+
+void Print(int[,] arr)
+{
+  int row = arr.GetLength(0);
+  int column = arr.GetLength(1);
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < column; j++)
+      Console.Write($" {arr[i, j]} ");
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+  int[,] arr = new int[row, column];
+
+  for (int i = 0; i < row; i++)
+    for (int j = 0; j < column; j++)
+      arr[i, j] = new Random().Next(from, to);
+
+  return arr;
+}
+
+string Example(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      while ()
+    }
+  }
+  return ;
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+string summ = Example(mass);
+Console.WriteLine(summ);
