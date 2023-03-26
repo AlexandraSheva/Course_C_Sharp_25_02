b3f1b3ea4ba9f1efce1baab9be3b5ea3e0a32064
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-03-26 16:55:21 +0300

    Task_9_3

---
+++ lesson_9/Task_9_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+
+
+int Stepen(int A, int B)
+{
+  if (B == 0) return 1;
+  return Stepen (A, B-1)*A;
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Stepen(num_1, num_2));
